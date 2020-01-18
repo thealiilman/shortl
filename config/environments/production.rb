5dcb488ad2dc3fefb4b93fd8e298aa968378abff
thealiilman <aliilman532@gmail.com>
2020-01-18 16:50:50 +0800

    Renamed app from Shortrl to Shortl.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "shortrl_production"
+  # config.active_job.queue_name_prefix = "shortl_production"
 
   config.action_mailer.perform_caching = false
 
