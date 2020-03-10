5c2f8c30121cfbf4352310a7b60d1544e8412d90
thealiilman <aliilman532@gmail.com>
2020-03-10 09:33:29 +0800

    Refactored and generated latest swagger doc.

--- spec/requests/public/links_controller_spec.rb
+++ spec/requests/public/links_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Public::LinksController, type: :request do
   describe "GET #show" do
     context 'when link exists' do
-      let!(:link) { create(:link) }
+      let(:link) { create(:link) }
 
       it "redirects to the link" do
         get public_link_path(key: link.key)
